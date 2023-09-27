f4c11427681ff659fee6bf5de4b889362f9f5fbd
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2023-09-27 09:15:25 +0545

    miles to km day-27

---
+++ 100daysofcode/day 27/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("my first gui program")
+window.minsize(500,300)
+my_label=tkinter.Label(text="i am a label",font=("arial",24,"italic"))
+my_label.pack()
+
+
+
+
+
+
+
+
+window.mainloop()
